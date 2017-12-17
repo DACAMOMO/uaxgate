ba20b6980292d5cc7fc2f13ea50ead541bdd7e6f
david <dacamomo_@hotmail.com>
2017-12-17 17:40:55 +0100

    isa prueba

--- ISA.py
+++ ISA.py
@@ -1,2 +1,19 @@
 import numpy as np
 
+#DATOS
+T0 = 288.15 #valor de la temperatura en K a nivel del mar
+P0 = 101325 #valor de la presión en Pa a nivel del mar
+rho0 = 1.225 #valor de la densidad en kg/m3 a nivel del mar
+
+
+
+#CONDICIÓN DE VUELO 0m<h<11000m
+
+T = T0 - (alpha*h)
+
+P = P0 * (1 - ( (h * alpha) / T0))^((g / R * alpha))
+
+rho = rho0 * (1 - ( (h * alpha) / T0))^((g / R * alpha) - 1)
+
+
+
